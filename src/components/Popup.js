f8f5f4bcd461299b2ad7ccda32e37393135eb7ce
seoyeon89 <90161145+seoyeon89@users.noreply.github.com>
2024-10-23 21:15:13 +0900

    스크롤 영역 커스텀

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,14 +6,14 @@ export const preventScroll = () => {
     document.body.style.position = 'fixed';
     document.body.style.width = '100%';
     document.body.style.top = `-${currentScrollY}px`; // 현재 스크롤 위치
-    document.body.style.overflowY = 'scroll';
+    // document.body.style.overflowY = 'scroll';
     return currentScrollY;
 };
 const allowScroll = (prevScrollY) => {
     document.body.style.position = '';
     document.body.style.width = '';
     document.body.style.top = '';
-    document.body.style.overflowY = '';
+    // document.body.style.overflowY = '';
     window.scrollTo(0, prevScrollY);
 };
 export const Popup = ({children, title}) => {
