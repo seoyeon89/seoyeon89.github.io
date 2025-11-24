75c515158f126429b4c857ab34376040955c5a20
seoyeon <seoyeon8937@gmail.com>
2025-11-24 13:18:38 +0900

    vite 배포로 변경

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => ({
-    plugins: [
-        react(),
-        svgr(),
-    ],
-    base: mode === "production" ? "/seoyeon89.github.io/" : "/",
-}));
+export default defineConfig({
+    plugins: [react(), svgr()],
+    base: "https://seoyeon89.github.io/",
+});
