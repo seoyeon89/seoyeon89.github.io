f9b8a945fd1bb27571f3800738132cb8e03caf4b
seoyeon <seoyeon8937@gmail.com>
2025-11-24 13:30:54 +0900

    vite 배포로 변경

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
-
 import './assets/css/style.scss';
-
-const base = import.meta.env.MODE === 'production'
-    ? '/seoyeon89.github.io/'
-    : '/';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter basename={base}>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
 
 reportWebVitals();
