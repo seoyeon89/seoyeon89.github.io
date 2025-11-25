d4030d949ba9e90eb642cf73367c8216f2751484
seoyeon <seoyeon8937@gmail.com>
2025-11-25 16:55:33 +0900

    신규 퍼블리싱 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
 
-import AppHeader from '../components/AppHeader';
+// import AppHeader from '../components/AppHeader';
 import Profile from '../components/Profile';
 import WorkList from '../components/WorkList';
 
 const Home = () => {
     return (
         <>
-            <AppHeader/>
+            {/*<AppHeader/>*/}
             <main className="container" id="profile">
                 <div className="container__background"></div>
                 <div className="container__inner">
